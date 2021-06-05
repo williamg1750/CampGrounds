6f8e6260e1155cdf3a621aa44f82e4def944781d
williamg1750 <william.guan12@gmail.com>
2021-06-05 09:38:30 -0400

    show

--- app.js
+++ app.js
@@ -24,13 +24,24 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({
-    title: 'My Backyard',
-    description: 'Cheap Camping',
-  });
-  await camp.save();
-  res.send(camp);
+// app.get('/makecampground', async (req, res) => {
+//   const camp = new Campground({
+//     title: 'My Backyard',
+//     description: 'Cheap Camping',
+//   });
+//   await camp.save();
+//   res.send(camp);
+// });
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/show', { campground });
 });
 
 app.listen(3000, () => {
