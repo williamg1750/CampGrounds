683b855935bd2847cf4c9ebdb24d541f8b3d7b3a
williamg1750 <william.guan12@gmail.com>
2021-06-05 12:05:42 -0400

    edit campground crud

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -20,28 +21,50 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
   res.render('home');
 });
-
-// app.get('/makecampground', async (req, res) => {
-//   const camp = new Campground({
-//     title: 'My Backyard',
-//     description: 'Cheap Camping',
-//   });
-//   await camp.save();
-//   res.send(camp);
-// });
 
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  // const { params } = req.body.campgrounds;
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`);
+  // res.send(req.body);
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
   const campground = await Campground.findById(id);
   res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const id = req.params.id;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const id = req.params.id;
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    req.body.campground
+  );
+  await campground.save();
+  res.redirect('campgrounds/show', { campground });
 });
 
 app.listen(3000, () => {
