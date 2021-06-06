254de50b730ec8cbc38033867bd90c46a48af385
williamg1750 <william.guan12@gmail.com>
2021-06-06 10:37:41 -0400

    bootstrap forms

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = await new Campground({
       title: `${random(descriptors)} ${random(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est neque similique eligendi quae, magni consectetur ea vel vitae eveniet ipsam!',
+      price,
     });
     await camp.save();
   }
