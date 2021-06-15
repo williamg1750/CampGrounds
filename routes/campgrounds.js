63439150b2b4343e5d3e1d6afdf8fe14ceedfa3c
williamg1750 <william.guan12@gmail.com>
2021-06-15 14:52:57 -0400

    flash alert for review and campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -63,6 +63,7 @@ router.put(
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
